20746d31e8f3ba7a832e7c501e85fc748b828746
Mariusz Kerl <mariuszkerl@gamil.com>
2017-08-27 20:30:58 +0100

    Added missing mapping extension methods for normal routes

--- src/Microsoft.AspNetCore.Routing.Subdomain/Microsoft.AspNetCore.Builder/MapSubdomainRouteRouteBuilderExtensions.cs
+++ src/Microsoft.AspNetCore.Routing.Subdomain/Microsoft.AspNetCore.Builder/MapSubdomainRouteRouteBuilderExtensions.cs
@@ -17,20 +17,28 @@ namespace Microsoft.AspNetCore.Builder
         {
             return MapSubdomainRoute(routeBuilder, hostnames, name, subdomain, template, defaults, constraints, dataTokens: null);
         }
-        public static IRouteBuilder MapRoute(this IRouteBuilder routeBuilder, string[] hostnames, string name, string template, object defaults)
-        {
-            routeBuilder.Routes.Add(new SubDomainRoute(hostnames, null, routeBuilder.DefaultHandler, name, template, new RouteValueDictionary(defaults), null, null,
-                new DefaultInlineConstraintResolver((IOptions<RouteOptions>)routeBuilder.ServiceProvider.GetService(typeof(IOptions<RouteOptions>)))));
-
-            return routeBuilder;
-        }
-
         public static IRouteBuilder MapSubdomainRoute(this IRouteBuilder routeBuilder, string[] hostnames, string name, string subdomain, string template, object defaults, object constraints, object dataTokens)
         {
             routeBuilder.Routes.Add(new SubDomainRoute(hostnames, subdomain, routeBuilder.DefaultHandler, name, template, new RouteValueDictionary(defaults), new RouteValueDictionary(constraints), new RouteValueDictionary(dataTokens),
                 new DefaultInlineConstraintResolver((IOptions<RouteOptions>)routeBuilder.ServiceProvider.GetService(typeof(IOptions<RouteOptions>)))));
 
             return routeBuilder;
+        }
+        public static IRouteBuilder MapRoute(this IRouteBuilder routeBuilder, string[] hostnames, string name, string template)
+        {
+            return MapRoute(routeBuilder, hostnames, name, template, defaults: null);
+        }
+        public static IRouteBuilder MapRoute(this IRouteBuilder routeBuilder, string[] hostnames, string name, string template, object defaults)
+        {
+            return MapRoute(routeBuilder, hostnames, name, template, defaults, constraints: null);
+        }        
+        public static IRouteBuilder MapRoute(this IRouteBuilder routeBuilder, string[] hostnames, string name, string template, object defaults, object constraints)
+        {
+            return MapRoute(routeBuilder, hostnames, name, template, defaults, constraints, dataTokens: null);
+        }
+        public static IRouteBuilder MapRoute(this IRouteBuilder routeBuilder, string[] hostnames, string name, string template, object defaults, object constraints, object dataTokens)
+        {
+            return MapSubdomainRoute(routeBuilder, hostnames, name, null, template, defaults, constraints, dataTokens);
         }
     }
 }

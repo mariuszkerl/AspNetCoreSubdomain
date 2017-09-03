faff4eaa6dd62a8288d06566cd5602ec18a8e4cc
Mariusz Kerl <mariuszkerl@gamil.com>
2017-09-03 13:33:40 +0100

    Removed commented code

--- src/Microsoft.AspNetCore.Routing.Subdomain/Microsoft.AspNetCore.Mvc.Routing/SubdomainUrlHelper.cs
+++ src/Microsoft.AspNetCore.Routing.Subdomain/Microsoft.AspNetCore.Mvc.Routing/SubdomainUrlHelper.cs
@@ -28,25 +28,6 @@ namespace Microsoft.AspNetCore.Routing.Subdomain.Microsoft.AspNetCore.Mvc.Routin
             _routeValueDictionary = new RouteValueDictionary();
         }
 
-        ///// <inheritdoc />
-        //public ActionContext ActionContext { get; }
-
-        ///// <summary>
-        ///// Gets the <see cref="RouteValueDictionary"/> associated with the current request.
-        ///// </summary>
-        //protected RouteValueDictionary AmbientValues => ActionContext.RouteData.Values;
-
-        ///// <summary>
-        ///// Gets the <see cref="Http.HttpContext"/> associated with the current request.
-        ///// </summary>
-        //protected HttpContext HttpContext => ActionContext.HttpContext;
-
-        ///// <summary>
-        ///// Gets the top-level <see cref="IRouter"/> associated with the current request. Generally an
-        ///// <see cref="IRouteCollection"/> implementation.
-        ///// </summary>
-        //protected IRouter Router => ActionContext.RouteData.Routers[0];
-
         /// <inheritdoc />
         public override string Action(UrlActionContext actionContext)
         {
@@ -119,23 +100,6 @@ namespace Microsoft.AspNetCore.Routing.Subdomain.Microsoft.AspNetCore.Mvc.Routin
             }
             return GenerateUrl(routeContext.Protocol, routeContext.Host, pathData, routeContext.Fragment);
         }
-
-        ///// <summary>
-        ///// Gets the <see cref="VirtualPathData"/> for the specified <paramref name="routeName"/> and route
-        ///// <paramref name="values"/>.
-        ///// </summary>
-        ///// <param name="routeName">The name of the route that is used to generate the <see cref="VirtualPathData"/>.
-        ///// </param>
-        ///// <param name="values">
-        ///// The <see cref="RouteValueDictionary"/>. The <see cref="Router"/> uses these values, in combination with
-        ///// <see cref="AmbientValues"/>, to generate the URL.
-        ///// </param>
-        ///// <returns>The <see cref="VirtualPathData"/>.</returns>
-        //protected virtual VirtualPathData GetVirtualPathData(string routeName, RouteValueDictionary values)
-        //{
-        //    var context = new VirtualPathContext(HttpContext, AmbientValues, values, routeName);
-        //    return Router.GetVirtualPath(context);
-        //}
 
         //// Internal for unit testing.
         //internal void AppendPathAndFragment(StringBuilder builder, VirtualPathData pathData, string fragment)
@@ -243,96 +207,5 @@ namespace Microsoft.AspNetCore.Routing.Subdomain.Microsoft.AspNetCore.Mvc.Routin
 
             return _stringBuilder;
         }
-
-        ///// <summary>
-        ///// Generates the URL using the specified components.
-        ///// </summary>
-        ///// <param name="protocol">The protocol for the URL, such as "http" or "https".</param>
-        ///// <param name="host">The host name for the URL.</param>
-        ///// <param name="pathData">The <see cref="VirtualPathData"/>.</param>
-        ///// <param name="fragment">The fragment for the URL.</param>
-        ///// <returns>The generated URL.</returns>
-        //protected virtual string GenerateUrl(string protocol, string host, VirtualPathData pathData, string fragment)
-        //{
-        //    if (pathData == null)
-        //    {
-        //        return null;
-        //    }
-
-        //    // VirtualPathData.VirtualPath returns string.Empty instead of null.
-        //    Debug.Assert(pathData.VirtualPath != null);
-
-        //    // Perf: In most of the common cases, GenerateUrl is called with a null protocol, host and fragment.
-        //    // In such cases, we might not need to build any URL as the url generated is mostly same as the virtual path available in pathData.
-        //    // For such common cases, this FastGenerateUrl method saves a string allocation per GenerateUrl call.
-        //    string url;
-        //    if (TryFastGenerateUrl(protocol, host, pathData, fragment, out url))
-        //    {
-        //        return url;
-        //    }
-
-        //    var builder = GetStringBuilder();
-        //    try
-        //    {
-        //        if (string.IsNullOrEmpty(protocol) && string.IsNullOrEmpty(host))
-        //        {
-        //            AppendPathAndFragment(builder, pathData, fragment);
-        //            // We're returning a partial URL (just path + query + fragment), but we still want it to be rooted.
-        //            if (builder.Length == 0 || builder[0] != '/')
-        //            {
-        //                builder.Insert(0, '/');
-        //            }
-        //        }
-        //        else
-        //        {
-        //            protocol = string.IsNullOrEmpty(protocol) ? "http" : protocol;
-        //            builder.Append(protocol);
-
-        //            builder.Append("://");
-
-        //            host = string.IsNullOrEmpty(host) ? HttpContext.Request.Host.Value : host;
-        //            builder.Append(host);
-        //            AppendPathAndFragment(builder, pathData, fragment);
-        //        }
-
-        //        var path = builder.ToString();
-        //        return path;
-        //    }
-        //    finally
-        //    {
-        //        // Clear the StringBuilder so that it can reused for the next call.
-        //        builder.Clear();
-        //    }
-        //}
-
-        //private bool TryFastGenerateUrl(
-        //    string protocol,
-        //    string host,
-        //    VirtualPathData pathData,
-        //    string fragment,
-        //    out string url)
-        //{
-        //    var pathBase = HttpContext.Request.PathBase;
-        //    url = null;
-
-        //    if (string.IsNullOrEmpty(protocol)
-        //        && string.IsNullOrEmpty(host)
-        //        && string.IsNullOrEmpty(fragment)
-        //        && !pathBase.HasValue)
-        //    {
-        //        if (pathData.VirtualPath.Length == 0)
-        //        {
-        //            url = "/";
-        //            return true;
-        //        }
-        //        else if (pathData.VirtualPath.StartsWith("/", StringComparison.Ordinal))
-        //        {
-        //            url = pathData.VirtualPath;
-        //            return true;
-        //        }
-        //    }
-
-        //    return false;
-        //}
     }
 }

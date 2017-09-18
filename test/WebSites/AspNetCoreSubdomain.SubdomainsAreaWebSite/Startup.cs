7af35cd6a45feeaf986cbed3387a4b6c36e82651
Mariusz Kerl <mariuszkerl@gmail.com>
2017-09-18 17:53:23 +0200

    Cleanup

--- test/WebSites/AspNetCoreSubdomain.SubdomainsAreaWebSite/Startup.cs
+++ test/WebSites/AspNetCoreSubdomain.SubdomainsAreaWebSite/Startup.cs
@@ -13,16 +13,6 @@ namespace AspNetCoreSubdomain.SubdomainsAreaWebSite
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env)
-        {
-            // var builder = new ConfigurationBuilder()
-            //     .SetBasePath(env.ContentRootPath)
-            //     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-            //     .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-            //     .AddEnvironmentVariables();
-            // Configuration = builder.Build();
-        }
-
         public IConfigurationRoot Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
